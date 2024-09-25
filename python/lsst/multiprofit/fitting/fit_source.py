aa9515e59e5491f4ef694df766b6982a544ebf18
Dan Taranu <dan.s.taranu@gmail.com>
2024-09-25 18:30:07 -0400

    Validate catexp channel order
    This occurs later than it should but is better than nothing.

--- python/lsst/multiprofit/fitting/fit_source.py
+++ python/lsst/multiprofit/fitting/fit_source.py
@@ -553,6 +553,11 @@ class CatalogSourceFitterABC(ABC, pydantic.BaseModel):
         )
 
         channels = self.get_channels(catexps)
+        if list(channels.values()) != config_data.channels:
+            raise ValueError(
+                f"self.get_channels(catexps)={channels} != {config_data.channels=};"
+                f" config_data.channels must match the order of channels in catexps"
+            )
         model_sources, priors = config_data.sources_priors
         # TODO: If free Observation params are ever supported, make null Data
         # Because config_data knows nothing about the Observation(s)
