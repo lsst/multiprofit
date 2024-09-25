4125199f63fb46552949fb2f9309d01d025a0491
Dan Taranu <dan.s.taranu@gmail.com>
2024-09-25 18:47:21 -0400

    Add plot_model_singleband

--- python/lsst/multiprofit/plotting/__init__.py
+++ python/lsst/multiprofit/plotting/__init__.py
@@ -3,6 +3,7 @@ from .errorvalues import *
 from .plot_catalog_bootstrap import *
 from .plot_loglike import *
 from .plot_model_rgb import *
+from .plot_model_singleband import *
 from .plot_sersicmix_interp import *
 from .reference_data import *
 from .types import *
